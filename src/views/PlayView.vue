15f61be71574274499d744002e914b196a73eb73
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-22 01:22:29 +0800

    fix: function not working when click outside word 'Start'

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -7,8 +7,8 @@ const flagStore = useFlagStore()
 
 <template>
   <div class="mt-1" v-if="!flagStore.showCountdown">
-    <div class="start-btn">
-      <p class="pixelify-sans" @click="flagStore.startGame">Start</p>
+    <div class="start-btn" @click="flagStore.startGame">
+      <p class="pixelify-sans">Start</p>
     </div>
   </div>
   <p v-if="!flagStore.showCountdown" class="description text-white mt-1 text-to-center">Guess as many flags as you can to become an ultimate flag master!</p>
