65887333c849d1fc6f946e907f3192f5967a6a0b
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-21 19:30:28 +0800

    chore: initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,14 @@
+<scrip setup lang="ts">
+
+</scrip>
+
+<template>
+    <main>
+        <nav>
+            <ul>
+                <li>Play</li>
+                <li>Instructions</li>
+            </ul>
+        </nav>
+    </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import logo from "@/assets/images/flagsprint.png"
+import Menu from "@/components/Menu.vue";
+</script>
+
+<template>
+  <main>
+    <div class="flex items-center flex-col justify-center">
+      <img :src="logo" alt="" width="200" height="80">
+      <!-- <p style="font-size:18px;">Can you guess what flag is this? 🚩</p> -->
+       <Menu />
+    </div>
+  </main>
+</template>
