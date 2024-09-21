b36be99b88b3c1d16389ccb1ea001d4c2bed10dd
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-22 01:12:08 +0800

    feat: add play view component

---
+++ src/views/PlayView.vue
@@ -0,0 +1,66 @@
+<style scoped>
+.countdown {
+  opacity: 1;
+  position: relative;
+  animation-name: slide;
+  animation-duration: 1s;
+}
+
+@keyframes slide {
+  0% { left: 0px; top: 0px; opacity: 0.8; }
+  25% { left: 100px; top: 0px; opacity: 0.6; }
+  50% { left: 130px; top: 0px; opacity: 0.4; }
+  75% { left: 150px; top: 0px; opacity: 0.2; }
+  100% { left: 180px; top: 0px; opacity: 0; }
+}
+</style>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const countDown = ref<number>(3);
+const showCountdown = ref<boolean>(false);
+
+const displayCountdown = () => {
+  const container = document.querySelector("#countdownContainer");
+  if (container) {
+    // Remove the animation class
+    container.classList.remove('countdown');
+
+    // Use a short delay before re-adding the class
+    setTimeout(() => {
+      container.classList.add('countdown');
+    }, 50); // Delay to allow the class removal to take effect
+  }
+};
+
+const startGame = () => {
+  showCountdown.value = true;
+
+ 
+  setTimeout(() => {
+    displayCountdown();
+  }, 100); 
+
+  let countdownInterval = setInterval(() => {
+    if (countDown.value > 1) {
+      displayCountdown();
+      countDown.value--;
+    } else {
+      clearInterval(countdownInterval);
+      showCountdown.value = false; 
+      countDown.value = 3; 
+    }
+  }, 1000); 
+};
+</script>
+
+<template>
+  <div class="mt-1" v-if="!showCountdown">
+    <div class="start-btn">
+      <p class="pixelify-sans" @click="startGame">Start</p>
+    </div>
+  </div>
+  <p v-if="!showCountdown" class="description text-white mt-1 text-to-center">Guess as many flags as you can to become an ultimate flag master!</p>
+  <p v-else class="pixelify-sans fs-60" id="countdownContainer">{{ countDown }}</p>
+</template>
