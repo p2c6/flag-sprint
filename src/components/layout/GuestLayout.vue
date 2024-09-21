faeabc45a507334821d5393e6f28c0181c162e65
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-22 01:12:21 +0800

    feat: add guest layout component

---
+++ src/components/layout/GuestLayout.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Menu from '@/components/Menu.vue';
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+    <main class="main">
+    <div class="flex flex-col justify-center items-center">
+        <div class="flex text-logo pixelify-sans fw-400"> 
+            <h1>F</h1>
+            <h1>l</h1>
+            <h1>a</h1>
+            <h1>g</h1>
+            <h1>S</h1>
+            <h1>p</h1>
+            <h1>r</h1>
+            <h1>i</h1>
+            <h1>n</h1>
+            <h1>t</h1>
+        </div>
+    </div>
+    <main class="flex flex-col justify-center items-center">
+        <RouterView />
+    </main>
+  </main>
+</template>
