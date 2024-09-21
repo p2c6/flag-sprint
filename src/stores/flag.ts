ff1c5c86ea96ca94f6fb575f88b6c0238259a530
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-22 01:21:18 +0800

    ref: move countdown login to store

--- src/stores/flag.ts
+++ src/stores/flag.ts
@@ -3,8 +3,44 @@ import { ref } from "vue";
 
 
 export const useFlagStore = defineStore('flag', () => {
-   
+    const countDown = ref<number>(3);
+    const showCountdown = ref<boolean>(false);
+
+    const displayCountdown = () => {
+    const container = document.querySelector("#countdownContainer");
+    if (container) {
+        // Remove the animation class
+        container.classList.remove('countdown');
+
+        // Use a short delay before re-adding the class
+        setTimeout(() => {
+        container.classList.add('countdown');
+        }, 50); // Delay to allow the class removal to take effect
+    }
+    };
+
+    const startGame = () => {
+    showCountdown.value = true;
+
+    setTimeout(() => {
+        displayCountdown();
+    }, 100); 
+
+    let countdownInterval = setInterval(() => {
+        if (countDown.value > 1) {
+        displayCountdown();
+        countDown.value--;
+        } else {
+        clearInterval(countdownInterval);
+        showCountdown.value = false; 
+        countDown.value = 3; 
+        }
+    }, 1000); 
+    };
+
     return {
-       
+        startGame,
+        countDown,
+        showCountdown
     }
 });
--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,48 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { useFlagStore } from '@/stores/flag';
 
-const countDown = ref<number>(3);
-const showCountdown = ref<boolean>(false);
+const flagStore = useFlagStore()
 
-const displayCountdown = () => {
-  const container = document.querySelector("#countdownContainer");
-  if (container) {
-    // Remove the animation class
-    container.classList.remove('countdown');
-
-    // Use a short delay before re-adding the class
-    setTimeout(() => {
-      container.classList.add('countdown');
-    }, 50); // Delay to allow the class removal to take effect
-  }
-};
-
-const startGame = () => {
-  showCountdown.value = true;
-
-  setTimeout(() => {
-    displayCountdown();
-  }, 100); 
-
-  let countdownInterval = setInterval(() => {
-    if (countDown.value > 1) {
-      displayCountdown();
-      countDown.value--;
-    } else {
-      clearInterval(countdownInterval);
-      showCountdown.value = false; 
-      countDown.value = 3; 
-    }
-  }, 1000); 
-};
 </script>
 
 <template>
-  <div class="mt-1" v-if="!showCountdown">
+  <div class="mt-1" v-if="!flagStore.showCountdown">
     <div class="start-btn">
-      <p class="pixelify-sans" @click="startGame">Start</p>
+      <p class="pixelify-sans" @click="flagStore.startGame">Start</p>
     </div>
   </div>
-  <p v-if="!showCountdown" class="description text-white mt-1 text-to-center">Guess as many flags as you can to become an ultimate flag master!</p>
-  <p v-else class="pixelify-sans fs-60" id="countdownContainer">{{ countDown }}</p>
+  <p v-if="!flagStore.showCountdown" class="description text-white mt-1 text-to-center">Guess as many flags as you can to become an ultimate flag master!</p>
+  <p v-else class="pixelify-sans fs-60" id="countdownContainer">{{ flagStore.countDown }}</p>
 </template>
