d910983c9ce0fab9714fa4207635a3f96296c961
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-21 19:44:52 +0800

    style: add pixelated font

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,18 @@ import Menu from "@/components/Menu.vue";
 <template>
   <main>
     <div class="flex items-center flex-col justify-center">
-      <img :src="logo" alt="" width="200" height="80">
+      <div class="flex"> 
+        <h1 class="pixelify-sans">F</h1>
+        <h1 class="pixelify-sans">l</h1>
+        <h1 class="pixelify-sans">a</h1>
+        <h1 class="pixelify-sans">g</h1>
+        <h1 class="pixelify-sans">S</h1>
+        <h1 class="pixelify-sans">p</h1>
+        <h1 class="pixelify-sans">r</h1>
+        <h1 class="pixelify-sans">i</h1>
+        <h1 class="pixelify-sans">n</h1>
+        <h1 class="pixelify-sans">t</h1>
+      </div>
       <!-- <p style="font-size:18px;">Can you guess what flag is this? 🚩</p> -->
        <Menu />
     </div>
