c2e14a82c66c35094f800d2c5c38ba28dfe84c79
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-23 00:18:12 +0800

    feat: add generatation of question function (not done)

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,7 +1,69 @@
 <script setup lang="ts">
 import { useFlagStore } from '@/stores/flag';
+import { computed, onMounted, reactive, ref, watch } from 'vue';
+import countries from "../../flag.json";
 
 const flagStore = useFlagStore()
+
+const letter = ref<string>('')
+
+interface Question {
+  flagUrl: string,
+  country: string
+}
+
+const question = ref<Question>({
+  flagUrl: "https://flagsapi.com/PH/flat/64.png",
+  country: "Philippines"
+})
+
+const setupTextField = () => {
+  var fields = document.getElementsByClassName('textField')
+  Array.from(fields).forEach(function(field){
+    field.addEventListener("keyup", function(event) {
+      if (field.value.length == 1 && event.code !== "Backspace") {
+        if (field.nextElementSibling) {
+          field.nextElementSibling.focus()
+        }
+      }
+
+      if(event.code === "Backspace" && field.value.length == 0) {
+        if (field.previousElementSibling) {
+          field.previousElementSibling.focus()
+        }
+      }
+    })
+  })
+}
+
+const handleChangeInput = (event:any) => {
+    const fields = document.getElementsByClassName('textField');
+    letter.value = Array.from(fields).map((field: HTMLInputElement) => field.value).join('');
+}
+
+const generateQuestion = () => {
+  
+  const randomIndex:number = Math.floor(Math.random() * countries.length) + 1;
+  const questionToAnswer = countries[randomIndex];
+
+  question.value = {flagUrl: questionToAnswer.flagUrl, country: questionToAnswer.country};
+}
+
+const nextQuestion = () => generateQuestion();
+
+const checkInput = (value: string) => {
+  if (question.value.country.toLowerCase() == value.toLowerCase()) {
+    nextQuestion()
+  }
+}
+
+watch(letter, (newVal:string) => {
+  checkInput(newVal)
+})
+
+onMounted(() => {
+  setupTextField()
+})
 
 </script>
 
@@ -12,7 +74,7 @@ const flagStore = useFlagStore()
         <p class="pixelify-sans">Start</p>
       </div>
 
-      <div class="description text-white mt-1 text-to-center">Guess as many flags as you can to become an ultimate flag master!</div>
+      <div class="description text-white mt-1 text-to-center">💭 Guess as many flags as you can to become an ultimate flag master! 👑</div>
     </div>
     <div v-else>
       <div v-if="flagStore.countDown > 0 && !flagStore.showOnGameView" class="pixelify-sans fs-60" id="countdownContainer">{{ flagStore.countDown }}</div>
@@ -30,23 +92,26 @@ const flagStore = useFlagStore()
     <p class="text-white mt-1">Can you guess what flag is this? 🚩</p>
 
     <div class="flag-container">
-      <img src="https://flagsapi.com/PH/flat/64.png" alt="">
+      <img :src="question.flagUrl">
     </div>
-    <div class="flex">
-      <input type="text" class="textField">
-      <input type="text" class="textField">
-      <input type="text" class="textField">
-      <input type="text" class="textField">
-      <input type="text" class="textField">
-      <input type="text" class="textField">
-      <input type="text" class="textField">
-      <input type="text" class="textField">
+    <div class="flex" id="textfield-container">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
+      <input type="text" class="textField" maxlength="1" @input="event => handleChangeInput(event)">
     </div>
     <div class="playing-timer-container">
       <div class="flex flex-end mt-1">
         <div class="playing-timer">
           <p>
-            5.2
+            ⏰ 5.2 
           </p>
         </div>
       </div>
