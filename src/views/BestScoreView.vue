c3192060f63000aef84d96266102aafd37fa3cb8
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-26 20:41:22 +0800

    feat: add best score component view

---
+++ src/views/BestScoreView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useScoreStore } from '@/stores/score';
+import { onMounted } from 'vue';
+
+const store = useScoreStore()
+
+onMounted(() => {
+    store.getHighScore()
+})
+
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center">
+        <p class="pixelify-sans fs-36 text-white">
+            Best Score: 
+        </p>
+        <p class="text-white pixelify-sans">
+            🪙 {{ store.highScore }}
+        </p>
+        <RouterLink :to="{name: 'menu'}" style="margin-top: 100px;">
+        <a class="text-white pixelify-sans fs-18">
+          📋 Main Menu
+        </a>
+      </RouterLink>
+    </div>
+</template>
