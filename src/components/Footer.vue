28b735f384be2e22bb06182852165601651f87cf
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-26 22:00:39 +0800

    feat: add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <footer class="flex flex-col items-center justify-center mt-5">
+        <a href="https://github.com/p2c6/flag-sprint">
+        <div class="flex flex-col items-center justify-center">
+                <div id="github">
+                    🐺
+                </div>
+                <a class="text-light fs-12">Github</a>
+            </div>
+        </a>
+    </footer>
+</template>
