ec5e8062e9b84ea5ee6e36290102e9b7c331248a
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-26 22:01:33 +0800

    feat: add footer

--- src/views/MainMenuView.vue
+++ src/views/MainMenuView.vue
@@ -1,9 +1,11 @@
 <script setup lang="js">
+import Footer from '@/components/Footer.vue';
 import Menu from '@/components/Menu.vue';
 
 </script>
 <template>
-     <div>
+     <div class="flex flex-col items-center justify-center">
          <Menu />
+         <Footer />
      </div>
 </template>
