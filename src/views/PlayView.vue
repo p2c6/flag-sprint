91b524ec01a6590b6e0cba6422b88aeae5f79a4f
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-28 20:05:02 +0800

    chore: remove unuse import

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -2,8 +2,7 @@
 import Difficulty from '@/components/Difficulty.vue';
 import { useScoreStore } from '@/stores/score';
 import { useSetupStore } from '@/stores/setup';
-import { onMounted, onUnmounted } from 'vue';
-import { RouterLink } from 'vue-router';
+import { onUnmounted } from 'vue';
 import Countdown from '@/components/Countdown.vue';
 import OnGame from '@/components/OnGame.vue';
 import GameOver from '@/components/GameOver.vue';
@@ -18,48 +17,48 @@ onUnmounted(() => {
 </script>
 
 <template>
-<div class="flex- flex-col items-center justify-center">
+  <div class="flex- flex-col items-center justify-center">
 
-  <Difficulty 
-    :showDifficultyView="setupStore.showDifficultyView" 
-    :onClickDifficulty="setupStore.handleChangeDifficulty"
-  />
+    <Difficulty 
+      :showDifficultyView="setupStore.showDifficultyView" 
+      :onClickDifficulty="setupStore.handleChangeDifficulty"
+    />
 
-  <Countdown 
-    :showDifficultyView="setupStore.showDifficultyView"
-    :showCountdown="setupStore.showCountdown"
-    :handleStartGame="setupStore.startGame"
-    :countDown="setupStore.countDown"
-    :showOnGameView="setupStore.showOnGameView"
-    :countDownMessage="setupStore.countDownMessage"
-  />
+    <Countdown 
+      :showDifficultyView="setupStore.showDifficultyView"
+      :showCountdown="setupStore.showCountdown"
+      :handleStartGame="setupStore.startGame"
+      :countDown="setupStore.countDown"
+      :showOnGameView="setupStore.showOnGameView"
+      :countDownMessage="setupStore.countDownMessage"
+    />
 
-  <OnGame 
-    :showCountdown="setupStore.showCountdown"
-    :showOnGameView="setupStore.showOnGameView"
-    :isGameOver="setupStore.isGameOver"
-    :isGameDefeated="setupStore.isGameDefeated"
-    :score="scoreStore.score"
-    :flag="setupStore.question.flagUrl"
-    :textFieldCount="setupStore.textFieldCount"
-    :handleChangeInput="setupStore.handleChangeInput"
-    :timer="setupStore.timer"
-  />
+    <OnGame 
+      :showCountdown="setupStore.showCountdown"
+      :showOnGameView="setupStore.showOnGameView"
+      :isGameOver="setupStore.isGameOver"
+      :isGameDefeated="setupStore.isGameDefeated"
+      :score="scoreStore.score"
+      :flag="setupStore.question.flagUrl"
+      :textFieldCount="setupStore.textFieldCount"
+      :handleChangeInput="setupStore.handleChangeInput"
+      :timer="setupStore.timer"
+    />
 
-  <GameOver 
-    :isGameOver="setupStore.isGameOver"
-    :message="scoreStore.message"
-    :score="scoreStore.score"
-    :handleClearSetup="setupStore.handleClearSetup"
-  />
+    <GameOver 
+      :isGameOver="setupStore.isGameOver"
+      :message="scoreStore.message"
+      :score="scoreStore.score"
+      :handleClearSetup="setupStore.handleClearSetup"
+    />
 
-  <FinishGame 
-    :isGameDefeated="setupStore.isGameDefeated"
-    :message="scoreStore.message"
-    :score="scoreStore.score"
-    :handleClearSetup="setupStore.handleClearSetup"
-  />
+    <FinishGame 
+      :isGameDefeated="setupStore.isGameDefeated"
+      :message="scoreStore.message"
+      :score="scoreStore.score"
+      :handleClearSetup="setupStore.handleClearSetup"
+    />
 
-</div>
+  </div>
   
 </template>
