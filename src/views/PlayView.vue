c548bea78c20db59e0510446ebfc0a93daf1527d
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-28 20:03:02 +0800

    ref: move finish game view to component

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -7,6 +7,7 @@ import { RouterLink } from 'vue-router';
 import Countdown from '@/components/Countdown.vue';
 import OnGame from '@/components/OnGame.vue';
 import GameOver from '@/components/GameOver.vue';
+import FinishGame from '@/components/FinishGame.vue';
 
 const setupStore = useSetupStore()
 const scoreStore = useScoreStore()
@@ -52,20 +53,12 @@ onUnmounted(() => {
     :handleClearSetup="setupStore.handleClearSetup"
   />
 
-  <div id="finish-game" v-show="setupStore.isGameDefeated">
-    <div class="flex flex-col justify-center items-center">
-      <p class="text-white pixelify-sans" id="finish-game-text">You're crown as Flag Master! 👑</p>
-      <p class="text-white pixelify-sans mt-1" id="finish-game-message">{{ scoreStore.message }}</p>
-      <p class="text-white pixelify-sans"  id="finish-game-score"> 🪙 {{ scoreStore.score }}</p>
-      <div class="text-white pixelify-sans fs-18 clickable" id="finish-game-play-again" @click="setupStore.handleClearSetup">🕹️ Play Again</div>
-      <p class="text-white pixelify-sans fs-12" id="finish-game-share">Share this link with your friends and challenge them!</p>
-      <RouterLink :to="{name: 'menu'}" id="finish-game-main-menu">
-        <a class="text-white pixelify-sans fs-18">
-          📋 Main Menu
-        </a>
-      </RouterLink>
-    </div>
-  </div>
+  <FinishGame 
+    :isGameDefeated="setupStore.isGameDefeated"
+    :message="scoreStore.message"
+    :score="scoreStore.score"
+    :handleClearSetup="setupStore.handleClearSetup"
+  />
 
 </div>
   
