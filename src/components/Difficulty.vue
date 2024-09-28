349f681b5e21db608c8d5bd85dab4045ee5eb685
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-28 18:54:47 +0800

    feat: add difficulty component

---
+++ src/components/Difficulty.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface DifficultyProps {
+    showDifficultyView: boolean,
+    onClickDifficulty: (difficulty: string) => void;
+}
+
+const { 
+        showDifficultyView, 
+        onClickDifficulty
+    } = defineProps<DifficultyProps>();
+</script>
+
+<template>
+  <div id="difficulty" v-show="showDifficultyView">
+    <div class="flex flex-col items-center justify-center pixelify-sans">
+      <ul class="text-white fs-24">
+        <li @click="onClickDifficulty('easy')">🤠 EASY</li>
+        <li  @click="onClickDifficulty('hard')">🥴 HARD</li>
+        <li  @click="onClickDifficulty('all')">😎 DEFAULT (All)</li>
+      </ul>
+    </div>
+  </div>
+</template>
