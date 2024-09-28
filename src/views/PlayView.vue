00ce4a2f9ef0e43e2247b0356a6d9768b49f764c
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-28 19:40:34 +0800

    ref: move on game view to component

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -5,6 +5,7 @@ import { useSetupStore } from '@/stores/setup';
 import { onMounted, onUnmounted } from 'vue';
 import { RouterLink } from 'vue-router';
 import Countdown from '@/components/Countdown.vue';
+import OnGame from '@/components/OnGame.vue';
 
 const setupStore = useSetupStore()
 const scoreStore = useScoreStore()
@@ -31,41 +32,17 @@ onUnmounted(() => {
     :countDownMessage="setupStore.countDownMessage"
   />
 
-  <div id="playing" class="flex flex-col items-center justify-center" v-show="!setupStore.showCountdown && setupStore.showOnGameView && !setupStore.isGameOver && !setupStore.isGameDefeated">
-    <div class="playing-header">
-      <div class="flex justify-between items-center">
-        <RouterLink :to="{name:'menu'}">
-          <a class="text-light pixelify-sans">⬅️ Back</a>
-        </RouterLink>
-        <p class="text-light pixelify-sans">🪙 Score: {{ scoreStore.score }}</p>
-      </div>
-    </div>
-    <div id="playing-question">
-      <p class="text-white mt-1 text-center">Can you guess what flag is this? 🚩</p>
-    </div>
-
-    <div class="flag-container">
-      <img :src="setupStore.question.flagUrl">
-    </div>
-    <div class="flex flex-wrap" id="textfield-container">
-      <input v-for="field in setupStore.textFieldCount" 
-        :key="field"
-        type="text" 
-        class="textfield" 
-        maxlength="1" 
-        @input="event => setupStore.handleChangeInput(event)"
-      >
-    </div>
-    <div class="playing-timer-container">
-      <div class="flex flex-end mt-1">
-        <div class="playing-timer">
-          <p>
-            ⏰ {{ setupStore.timer }}
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <OnGame 
+    :showCountdown="setupStore.showCountdown"
+    :showOnGameView="setupStore.showOnGameView"
+    :isGameOver="setupStore.isGameOver"
+    :isGameDefeated="setupStore.isGameDefeated"
+    :score="scoreStore.score"
+    :flag="setupStore.question.flagUrl"
+    :textFieldCount="setupStore.textFieldCount"
+    :handleChangeInput="setupStore.handleChangeInput"
+    :timer="setupStore.timer"
+  />
 
   <div id="game-over" v-show="setupStore.isGameOver">
     <div class="flex flex-col justify-center items-center">
